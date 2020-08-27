6ed462185e29ee50070ce15c21c7402fe0afb2f7
Warley Máximo de Matos Pereira <warleym.matos@gmail.com>
2020-08-26 21:58:34 -0300

    Criando $_GET

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 //echo "boa noite!";
 
+/*
 $con = mysqli_connect(
     "localhost",
     "root",
@@ -14,9 +15,10 @@ echo "Conectou!!!";
 echo "<br>";
 echo "Informações do host: " .mysqli_get_host_info($con);
 mysqli_close($con);
+*/
 
-//$nome = $_GET["nome"];
-//echo "O Nome é: ".$nome;
+$nome = $_GET["nome"];
+echo "O Nome é: ".$nome;
 
 
 
