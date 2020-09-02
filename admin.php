f3c11a55aa905bb43d3ed7e92b8322410301d90c
Warley Máximo de Matos Pereira <warleym.matos@gmail.com>
2020-09-01 23:53:42 -0300

    Atividade painel

--- admin.php
+++ admin.php
@@ -77,10 +77,10 @@ session_start();
 
 <!-- Main component for a primary marketing message or call to action -->
 <div class="jumbotron">
-  <h1>Navbar example</h1>
-  <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+  <h1>Sistema de Pacientes da COVID - 19</h1>
+  <p>Bem vindo <?php echo($nome_usuario); ?>.</p>     
   <p>
-    <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+    <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Acesso &raquo;</a>
   </p>
 </div>
 
