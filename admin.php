8be5e9934f794a9fb7aeef41d1c6afa4bd6f3c42
Warley Máximo de Matos Pereira <warleym.matos@gmail.com>
2020-09-01 22:15:18 -0300

    Alterando Tamplate

--- admin.php
+++ admin.php
@@ -23,6 +23,8 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SysPacientes - ADM</title>
     <link rel="icon" href="img/favicon/favicon2.ico">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     
 </head>
 
