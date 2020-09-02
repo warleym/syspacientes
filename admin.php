ce99358623ee38fc305920be7a40ecb701524fc0
Warley Máximo de Matos Pereira <warleym.matos@gmail.com>
2020-09-01 23:28:17 -0300

    .

--- admin.php
+++ admin.php
@@ -53,6 +53,14 @@ session_start();
 
 
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">SysPacientes</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+
 <div class="container">
 
 <!-- Static navbar -->
