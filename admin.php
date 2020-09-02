beb9c363a40d3b2cb7467437024d3157abb47e57
Warley Máximo de Matos Pereira <warleym.matos@gmail.com>
2020-09-01 23:13:07 -0300

    alteração layout .

--- admin.php
+++ admin.php
@@ -104,8 +104,7 @@ session_start();
 </div> <!-- /container -->
 
 
-<!-- Bootstrap core JavaScript
-================================================== -->
+<!-- Bootstrap core JavaScript -->
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
 <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
